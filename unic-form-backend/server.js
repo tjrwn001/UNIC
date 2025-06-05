47ae9a7f4a0ff19e1f60bda18d272bb61598482e
tjrwn001 <107621101+tjrwn001@users.noreply.github.com>
2025-06-05 18:20:38 +0100

    Update server.js

--- unic-form-backend/server.js
+++ unic-form-backend/server.js
@@ -74,6 +74,6 @@ app.post('/submit', upload.fields([
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
